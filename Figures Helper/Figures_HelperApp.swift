7a1c0ba972563f4559db5b2cd1b3bcc0b9547e5f
Patrick Boren <Pboren@lcs.on.ca>
2021-12-01 09:53:43 -0500

    Initial Commit

---
+++ Figures Helper/Figures_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Figures_HelperApp.swift
+//  Figures Helper
+//
+//  Created by Patrick Boren on 2021-12-01.
+//
+
+import SwiftUI
+
+@main
+struct Figures_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
